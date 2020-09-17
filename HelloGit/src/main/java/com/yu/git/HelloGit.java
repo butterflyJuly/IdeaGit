512bc3f2943a9e20a1a26647cd00ad69db76ca0b
butterflyJuly <228311683@qq.com>
2020-09-17 12:21:33 +0800

    第一次使用IDEA提交到本地库

---
+++ HelloGit/src/main/java/com/yu/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.yu.git;
+
+/**
+ * @ClassName
+ * @Description TODO
+ * @Author yu
+ * @Date 2020/9/17 7:31
+ * 在idea中使用Git
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
